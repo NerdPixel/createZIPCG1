9485c746ef36c5ef5ae97b1ca1cb3bce1310b1a4
hjalmar <venondesign@gmail.com>
2020-11-05 17:37:32 +0100

    feat(): add outputfolder and print exception

--- createZip.py
+++ createZip.py
@@ -4,17 +4,19 @@ import re
 import shutil
 
 
-def do(firstName, lastName, path):
+def do(firstName, lastName, path, outputFolder):
     directory_contents = os.listdir(path)
     for item in directory_contents:
-        if os.path.isdir(path+"/"+item) and re.match("^(cg)\d+_{1}(exercise)_{1}\d{2}", item):
+        if os.path.isdir(path + "/" + item) and re.match("^(cg)\d+_{1}(exercise)_{1}\d{2}", item):
             try:
                 currentPath = "src/"
-                outputFilename = firstName + "_" + lastName + "_cg1_ex" + item.split("_")[-1]
+                outputFilename = outputFolder + firstName + "_" + lastName + "_cg1_ex" + item.split("_")[-1]
                 shutil.make_archive(outputFilename, 'zip', path + "/" + item + "/skeleton", currentPath)
-            except:
+            except Exception as e:
                 print("An exception occurred")
+                print(e)
 
 
 if __name__ == '__main__':
-    do(firstName="Max", lastName="Mustermann", path="../CG1")
+    do(firstName="Max", lastName="Mustermann", path=".", outputFolder="./out/")
+
