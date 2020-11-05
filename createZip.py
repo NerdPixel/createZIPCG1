5b5b0c08fe6ee72ab46318f6d0bab569666e4b15
nerdpixel <joh.hackler@gmail.com>
2020-11-05 16:29:54 +0100

    fix(path): check for dir

--- createZip.py
+++ createZip.py
@@ -7,7 +7,7 @@ import shutil
 def do(firstName, lastName, path):
     directory_contents = os.listdir(path)
     for item in directory_contents:
-        if os.path.isdir(item) and re.match("^(cg)\d+_{1}(exercise)_{1}\d{2}", item):
+        if os.path.isdir(path+"/"+item) and re.match("^(cg)\d+_{1}(exercise)_{1}\d{2}", item):
             try:
                 currentPath = "src/"
                 outputFilename = firstName + "_" + lastName + "_cg1_ex" + item.split("_")[-1]
@@ -17,4 +17,4 @@ def do(firstName, lastName, path):
 
 
 if __name__ == '__main__':
-    do(firstName="Max", lastName="Mustermann", path="../../WebstormProjects/CG1")
+    do(firstName="Max", lastName="Mustermann", path="/Users/johann/WebstormProjects/CG1")
